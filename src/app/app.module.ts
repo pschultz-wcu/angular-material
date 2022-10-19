1fbb36717b79921c68f4d02963661a1c8aa41be6
pSchultz <pschultz@westcoastuniversity.edu>
2022-10-19 12:51:47 -0700

    initialize ./components directory, move navbar buttons and form directories inside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MaterialComponentsModule } from '../material.module';
 
-import { VFormBtnGroupComponent } from './buttons/v-form-btn-group/v-form-btn-group.component';
-import { HFormBtnGroupComponent } from './buttons/h-form-btn-group/h-form-btn-group.component';
-import { ContinueBtnComponent } from   './buttons/continue-btn/continue-btn.component';
-import { TextFieldComponent } from './form-controls/text-field/text-field.component';
-import { SelectFieldComponent } from './form-controls/select-field/select-field.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { VFormBtnGroupComponent } from './components/buttons/v-form-btn-group/v-form-btn-group.component';
+import { HFormBtnGroupComponent } from './components/buttons/h-form-btn-group/h-form-btn-group.component';
+import { ContinueBtnComponent } from   './components/buttons/continue-btn/continue-btn.component';
+import { TextFieldComponent } from './components/form-controls/text-field/text-field.component';
+import { SelectFieldComponent } from './components/form-controls/select-field/select-field.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
  
 @NgModule({
   declarations: [
