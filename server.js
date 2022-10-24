6e3aca38135f85419c7c594c20df5904d6f58983
pSchultz <pschultz@westcoastuniversity.edu>
2022-10-24 09:40:49 -0700

    edit path to index.html in server.js to match path in heroku build

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/angular-material-components"));
+app.use(express.static(__dirname + "/dist/wcu-material"));
 app.get("/*", function (req, res) {
   res.sendFile(
-    path.join(__dirname + "/dist/angular-material-components/index.html")
+    path.join(__dirname + "/dist/wcu-material/index.html")
   );
 });
 app.listen(process.env.PORT || 8080);
