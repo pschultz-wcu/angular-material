45e1fdc16842b8e3ca9d302476b613a6f7373fe6
pSchultz <pschultz@westcoastuniversity.edu>
2022-10-26 10:23:41 -0700

    add warn states to expansion panel components

--- src/app/components/expansion-panel/expansion-panel.component.ts
+++ src/app/components/expansion-panel/expansion-panel.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+type PanelState = 'base' | 'no-program' | 'no-campus';
+let stateString: string = 'base';
+let panelState: PanelState = stateString as PanelState;
+
 @Component({
   selector: 'app-expansion-panel',
   templateUrl: './expansion-panel.component.html',
   styleUrls: ['./expansion-panel.component.scss'],
 })
 export class ExpansionPanelComponent implements OnInit {
+  panelState = panelState;
+  panelStateIsBase: PanelState = 'base';
+  panelStateIsNoProgram: PanelState = 'no-program';
+  panelStateIsNoCampus: PanelState = 'no-campus';
+
   panel1OpenState = false;
   panel2OpenState = false;
   panel3OpenState = false;
   panel4OpenState = false;
   panel5OpenState = false;
-  panel6OpenState = false;
-  panel7OpenState = false;
-  panel8OpenState = false;
 
   constructor() {}
 
